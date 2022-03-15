75d9618dafd21d30bb0cd1b02a18073730965441
Nicolo Trinca <58513377+trincaw@users.noreply.github.com>
2022-03-15 15:08:53 +0100

    Delete row col & add fix

--- TableData.cpp
+++ TableData.cpp
@@ -26,42 +26,48 @@ vector<vector<double>>* TableData::getTable() const{
     return table;
 }
 void TableData::addRow(const vector<double> &row,u_int index, const string &rowName){
-    if(rowsCount==0 && columnsCount==0){
+    if(row.size()==rowsCount){
+        if(rowsCount==0 && columnsCount==0){
         table->push_back(row);
         rowsNames->insert(rowsNames->begin()+index,rowName);
         columnsCount+=1;
-    }
-    else if(rowsCount>index){
-    rowsNames->insert(rowsNames->begin()+index,rowName);
-    table->insert(table->begin()+index,row);
-    }
-    else if(rowsCount==index){
-    rowsNames->push_back(rowName);
-    table->push_back(row);
+     }
+        else if(rowsCount>index){
+        rowsNames->insert(rowsNames->begin()+index,rowName);
+        table->insert(table->begin()+index,row);
+     }
+        else if(rowsCount==index){
+        rowsNames->push_back(rowName);
+        table->push_back(row);
+     }
+     rowsCount+=1;
     }
     else
-        std::cout <<"Add row out of range";
-    rowsCount+=1;
+        std::cout <<"Add row out of range or overflow"<< std::endl;
+
 }
 void TableData::addColumn(const vector<double> &column,u_int index, const string &columnName){
-    if(rowsCount==0 && columnsCount==0 ){
-        rowsCount+=1;
+    if(column.size()==columnsCount){
+        if(rowsCount==0 && columnsCount==0 ){
         rowsNames->insert(rowsNames->begin()+index,columnName);
         table->push_back(column);
-    }
-    else if(columnsCount>index){
+        rowsCount+=1;
+        }
+        else if(columnsCount>index){
              for(u_int i=0;columnsCount>i;++i)
                  (*table)[i].insert((*table)[i].begin()+index,column[i]);
              columnsNames->insert(rowsNames->begin()+index,columnName);
         }
-    else if(columnsCount==index){
+        else if(columnsCount==index){
              for(u_int i=0;columnsCount>i;++i)
                  (*table)[i].push_back(column[i]);
              columnsNames->push_back(columnName);
         }
-        else
-            std::cout <<"Add column out of range ";
-     columnsCount+=1;
+        columnsCount+=1;
+    }
+    else
+        std::cout <<"Add column out of range or overflow"<< std::endl;
+
 }
 vector<string>* TableData::getRowsNames() const{
     return rowsNames;
@@ -70,10 +76,23 @@ vector<string>* TableData::getColumnsNames() const{
     return columnsNames;
 }
 void TableData::deleteColumn(const u_int index){
-
+    if(columnsCount>index ){
+        columnsNames->erase(columnsNames->begin()+index);
+        for(u_int i=0;rowsCount>i;++i)
+            (*table)[i].erase((*table)[i].begin()+index);
+        columnsCount-=1;
+    }
+    else
+        std::cout <<"Remove column out of range or overflow"<< std::endl;
 }
 void TableData::deleteRow(const u_int index){
-
+    if(rowsCount>index ){
+    rowsNames->erase(rowsNames->begin()+index);
+    table->erase(table->begin()+index);
+    rowsCount-=1;
+    }
+    else
+        std::cout <<"Remove row out of range or overflow"<< std::endl;
 }
 
 
@@ -83,7 +102,7 @@ void TableData::printModel() const
 {
     for(auto data: *table){
         for(auto d: data)
-            std::cout << d;
+            std::cout << d << " ";
         std::cout << std::endl;
     }
     std::cout << "Columns:"<< columnsCount<<" ";
@@ -93,7 +112,7 @@ void TableData::loadModelRandom(const u_int columns,const u_int rows){
     for(u_int j=0;columns>j;j++){
         vector<double> d= vector<double>();
         for(u_int i=0;rows>i;i++){
-            d.push_back(0);
+            d.push_back(i+j);
         }
         table->push_back(d);
         columnsNames->push_back("");
--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 int main(int argc, char *argv[])
 {
     TableData* d=new  TableData();
-    d->loadModelRandom(5,5);
+    d->loadModelRandom(3,5);
     vector<double> temp= vector<double>{1,1,1,1,1};
-    d->addRow(temp,0);
+    d->addRow(temp,5);
     d->printModel();
 }
