7bdbca5bbb653f5795ac86a7cd23ce5829f2fce4
trincaw <nicoloxtrinca@gmail.com>
2022-05-11 11:18:19 +0200

    try

--- AllCharts.cpp
+++ AllCharts.cpp
@@ -1,14 +1,31 @@
 #include "AllCharts.h"
 
 void LineChart::setUp(Model* model,QVXYModelMapper *mapper){
+    //if(model->columnCount()%2==0)
+    //for(int i=0;i<model->columnCount();++i)
     mapper->setXColumn(0);
     mapper->setYColumn(1);
+
     mapper->setModel(model);
 
-    chart->setAnimationOptions(QChart::AllAnimations);
     QLineSeries *series = new QLineSeries;
-    series->setName("Line1");
-    mapper->setSeries(series);
+    series->setName("Line 1");
+    //mapper->setSeries(series);
+
     chart->addSeries(series);
     chart->createDefaultAxes();
+
+
+
+
+    series = new QLineSeries;
+    series->setName("Line 2");
+
+    mapper = new QVXYModelMapper();
+    mapper->setXColumn(2);
+    mapper->setYColumn(3);
+    mapper->setSeries(series);
+    mapper->setModel(model);
+    chart->addSeries(series);
+
 }
