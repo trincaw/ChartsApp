34f867a255e1626f4148d4146741e317dc0e807a
trincaw <nicoloxtrinca@gmail.com>
2022-05-24 12:16:13 +0200

    fix line chart

--- AllCharts.cpp
+++ AllCharts.cpp
@@ -8,7 +8,7 @@ QChart* BarChart::generateChart(TableData* table) const
     for (u_int i(0); i < table->getColumnCount(); i++) {
         QBarSet *set = new QBarSet("Bar set " + QString::number(i+1));
         for (u_int j=0;j < table->getRowCount();++j)
-            *set << table->getTable()->at(i).at(j);
+            *set << table->getTable()->at(j).at(i);
         series->append(set);
         chart->createDefaultAxes();
     }
@@ -49,7 +49,7 @@ QChart* LineChart::generateChart(TableData* table) const{
     for (u_int i(0); i < table->getColumnCount(); i++) {
         QLineSeries *series = new QLineSeries(chart);
         for (u_int j=0;j < table->getRowCount();j=j+2)
-            series->append(table->getTable()->at(i).at(j),table->getTable()->at(i).at(j+1));
+            series->append(table->getTable()->at(j).at(i),table->getTable()->at(j).at(i+1));
         series->setName(name + QString::number(nameIndex));
         nameIndex++;
         chart->addSeries(series);
@@ -62,9 +62,12 @@ QChart* SplineChart::generateChart(TableData* table) const{
     QChart *chart = new QChart();
     QString name("Series ");
     int nameIndex = 0;
-    for (u_int i(0); i < table->getColumnCount(); i++) {
+    u_int columns=table->getColumnCount();
+    if(!columns%2==0)
+        columns-=1;
+    for (u_int i(0); i < table->getRowCount(); ++i) {
         QSplineSeries *series = new QSplineSeries(chart);
-        for (u_int j=0;j < table->getRowCount();j=j+2)
+        for (u_int j=0;j < columns;j=j+2)
             series->append(table->getTable()->at(i).at(j),table->getTable()->at(i).at(j+1));
         series->setName(name + QString::number(nameIndex));
         nameIndex++;
@@ -74,6 +77,23 @@ QChart* SplineChart::generateChart(TableData* table) const{
     chart->createDefaultAxes();
     return chart;
 }
+//template<typename T>
+//void f(T s,TableData* table,QChart *chart)
+//{
+//    QString name("Series ");
+//    int nameIndex = 0;
+//    u_int columns=table->getColumnCount();
+//    if(!columns%2==0)
+//        columns-=1;
+//    for (u_int i(0); i < table->getRowCount(); ++i) {
+//        s = new T(chart);
+//        for (u_int j=0;j < columns;j=j+2)
+//            s->append(table->getTable()->at(i).at(j),table->getTable()->at(i).at(j+1));
+//        s->setName(name + QString::number(nameIndex));
+//        nameIndex++;
+//        chart->addSeries(s);
+//    }
+//}
 QChart* ScatterChart::generateChart(TableData* table) const{
     QChart *chart = new QChart();
     int nameIndex = 0;
@@ -81,7 +101,7 @@ QChart* ScatterChart::generateChart(TableData* table) const{
     for (u_int i(0); i < table->getColumnCount(); i++) {
         QScatterSeries *series = new QScatterSeries(chart);
         for (u_int j=0;j < table->getRowCount();j=j+2)
-            series->append(table->getTable()->at(i).at(j),table->getTable()->at(i).at(j+1));
+            series->append(table->getTable()->at(j).at(i),table->getTable()->at(j).at(i+1));
         series->setName(name + QString::number(nameIndex));
         nameIndex++;
         chart->addSeries(series);
