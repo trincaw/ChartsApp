0f8f48336bc38f76d6cc15dd990001fc5dedc643
trincaw <nikxtr@gmail.com>
2022-07-10 18:44:42 +0200

    zoom solo su line chart

--- ChartView.cpp
+++ ChartView.cpp
@@ -6,27 +6,30 @@ ChartView::ChartView() : QChartView(){
 }
 void ChartView::keyPressEvent(QKeyEvent *event)
 {
-    switch (event->key()) {
-    case Qt::Key_Plus:
-        chart()->zoomIn();
-        break;
-    case Qt::Key_Minus:
-        chart()->zoomOut();
-        break;
-    case Qt::Key_Left:
-        chart()->scroll(-10, 0);
-        break;
-    case Qt::Key_Right:
-        chart()->scroll(10, 0);
-        break;
-    case Qt::Key_Up:
-        chart()->scroll(0, 10);
-        break;
-    case Qt::Key_Down:
-        chart()->scroll(0, -10);
-        break;
-    default:
-        QGraphicsView::keyPressEvent(event);
-        break;
+    if (chart()->series().count()>1 && dynamic_cast<QXYSeries*>(chart()->series().at(0))) {
+        switch (event->key()) {
+        case Qt::Key_Plus:
+            chart()->zoomIn();
+            break;
+        case Qt::Key_Minus:
+            chart()->zoomOut();
+            break;
+        case Qt::Key_Left:
+            chart()->scroll(-10, 0);
+            break;
+        case Qt::Key_Right:
+            chart()->scroll(10, 0);
+            break;
+        case Qt::Key_Up:
+            chart()->scroll(0, 10);
+            break;
+        case Qt::Key_Down:
+            chart()->scroll(0, -10);
+            break;
+        default:
+            QGraphicsView::keyPressEvent(event);
+            break;
+        }
     }
+
 }
